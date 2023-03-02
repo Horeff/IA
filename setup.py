f844c30adee49d49ad85aa6b188cf9503efb625a
Horeff <60672323+Horeff@users.noreply.github.com>
2023-03-02 23:53:49 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    # Needed to silence warnings (and to be a worthwhile package)
+    name='IA',
+    author='Simon LEFRANC',
+    author_email='simon.lefranc94@gmail.com',
+    packages=find_packages()
+)
