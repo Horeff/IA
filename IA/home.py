cf647251f208afac11d537edcb82c4121b549141
Horeff <60672323+Horeff@users.noreply.github.com>
2023-03-03 17:44:37 +0100

    Update home.py

--- IA/home.py
+++ IA/home.py
@@ -1,5 +1,5 @@
 from IA import Neurone
-from Res_Neur.Network import Network
+from Res_Neur.Network import Network as Network
 from Res_Neur.conv_layer import ConvLayer
 from Res_Neur.activation_layer import ActivationLayer
 from Res_Neur.activations import tanh, tanh_prime
