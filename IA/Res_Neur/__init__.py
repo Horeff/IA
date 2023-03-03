cbaf36f2689f9a3ad0b26cba6fe6a4ea2ccedb8c
Horeff <60672323+Horeff@users.noreply.github.com>
2023-03-03 13:21:44 +0100

    Update __init__.py

--- IA/Res_Neur/__init__.py
+++ IA/Res_Neur/__init__.py
@@ -1,1 +1,7 @@
-
+from IA.Res_Neur import network
+from IA.Res_Neur.network import Network
+from IA.Res_Neur.conv_layer import ConvLayer
+from IA.Res_Neur.activation_layer import ActivationLayer
+from IA.Res_Neur.activations import tanh, tanh_prime
+from IA.Res_Neur.losses import mse, mse_prime
+from IA.Res_Neur.fc_layer import FCLayer
