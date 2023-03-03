7760e64d209e32b3643a34f86e0fce33370583ef
Horeff <60672323+Horeff@users.noreply.github.com>
2023-03-03 12:32:25 +0100

    Update __init__.py

--- IA/__init__.py
+++ IA/__init__.py
@@ -1,10 +1,10 @@
 from IA import Neurone, 
-from IA.res_neur import network
-from IA.Res_neur.network import Network
-from IA.Res_neur.conv_layer import ConvLayer
-from IA.Res_neur.activation_layer import ActivationLayer
-from IA.Res_neur.activations import tanh, tanh_prime
-from IA.Res_neur.losses import mse, mse_prime
+from IA.Res_Neur import network
+from IA.Res_Neur.network import Network
+from IA.Res_Neur.conv_layer import ConvLayer
+from IA.Res_Neur.activation_layer import ActivationLayer
+from IA.Res_Neur.activations import tanh, tanh_prime
+from IA.Res_Neur.losses import mse, mse_prime
 
 class IA_home:
   def __init__(self):
@@ -12,3 +12,17 @@ class IA_home:
   
   def create_res(self,*args):
     return Neurone.reseau(*args)
+  
+  def conv_res(self, layers, *args):
+    # network
+    net = Network()
+    for lay in layers:
+      net.add(ConvLayer(*lay))
+      net.add(ActivationLayer(tanh, tanh_prime))
+    # train
+    net.use(mse, mse_prime)
+    net.fit(*args)
+    return net
+  
+  def lstm_res(self, layers, *args):
+    net = Network()
