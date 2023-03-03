bfd8f3a4e1199678138ed24b595d59acf40ace4d
Horeff <60672323+Horeff@users.noreply.github.com>
2023-03-03 18:21:00 +0100

    Update __init__.py

--- IA/Res_neur/__init__.py
+++ IA/Res_neur/__init__.py
@@ -2,3 +2,7 @@ from IA.Res_neur.activations import *
 from IA.Res_neur.fc_layer import *
 from IA.Res_neur.layer import *
 from IA.Res_neur.losses import *
+from IA.Res_neur.activation_layer import *
+from IA.Res_neur.conv_layer import *
+from IA.Res_neur.flatten_layer import *
+from IA.Res_neur.lstm import *
