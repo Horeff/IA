6c259a07475d5f27e3b87e06a6a252dac05c9fee
Horeff <60672323+Horeff@users.noreply.github.com>
2023-03-03 17:46:15 +0100

    Update home.py

--- IA/home.py
+++ IA/home.py
@@ -1,10 +1,10 @@
 from IA import Neurone
-from Res_Neur.Network import Network as Network
-from Res_Neur.conv_layer import ConvLayer
-from Res_Neur.activation_layer import ActivationLayer
-from Res_Neur.activations import tanh, tanh_prime
-from Res_Neur.losses import mse, mse_prime
-from Res_Neur.fc_layer import FCLayer
+from Network import Network
+from conv_layer import ConvLayer
+from activation_layer import ActivationLayer
+from activations import tanh, tanh_prime
+from losses import mse, mse_prime
+from fc_layer import FCLayer
 import numpy as np
 
 
