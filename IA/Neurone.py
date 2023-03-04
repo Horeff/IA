795a689bec21c76c616f48a6c265edd8da2601af
Horeff <60672323+Horeff@users.noreply.github.com>
2023-03-04 17:39:02 +0100

    Update Neurone.py

--- IA/Neurone.py
+++ IA/Neurone.py
@@ -176,9 +176,8 @@ class multiclass_reseau:
         for res in self.reseaux:
             prediction = np.concatenate((prediction, res.predict(X)[0]))
         indexes = np.where(prediction == 1)[0]
-        print(indexes, prediction)
         if len(indexes) == 0:
-            return self.classes[-1]
+            return [self.classes[-1]]
         else:
             return [self.classes[i] for i in indexes]
             
