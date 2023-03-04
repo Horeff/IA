cf699bb4d0fec028e4329590184b829245a8fb40
Horeff <60672323+Horeff@users.noreply.github.com>
2023-03-04 11:12:44 +0100

    Update Neurone.py

--- IA/Neurone.py
+++ IA/Neurone.py
@@ -157,4 +157,4 @@ class reseau:
     def predict(self, X):
         activations = self.forward_propagation(X)
         Af = activations['A' + str(self.C)]
-        return round(Af)
+        return Af
